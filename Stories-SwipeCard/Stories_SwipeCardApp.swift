59d5fea9094f34fd1f98c2d2af134277d9cd789b
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-30 14:10:29 +0900

    Initial Commit

---
+++ Stories-SwipeCard/Stories_SwipeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stories_SwipeCardApp.swift
+//  Stories-SwipeCard
+//
+//  Created by Seungchul Ha on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct Stories_SwipeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
